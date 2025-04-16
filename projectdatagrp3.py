62f3260ef69cc2cc50df849f8d854e8ee7abfa05
Dinahak <101067107+Dinahak@users.noreply.github.com>
2025-04-16 11:40:55 -0500

    Update projectdatagrp3.py

--- projectdatagrp3.py
+++ projectdatagrp3.py
@@ -9,7 +9,7 @@ Original file is located at
 
 #https://raw.githubusercontent.com/Dinahak/Project-Data/main/Project_data.xlsx
 
-
+import streamlit as st
 import pandas as pd
 import requests
 from io import BytesIO
